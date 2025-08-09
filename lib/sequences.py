8c97686d8e8af7904c584859fbe9c6e267c89e91
Nicholas obonyo abala <nicholasobonyo444@gmail.com>
2025-08-09 16:21:07 +0300

    Lab complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
-def print_fibonacci(length):
-    pass
+def print_fibonacci(n):
+    fib_sequence = []
+    a, b = 0, 1
+    for _ in range(n):
+        fib_sequence.append(a)
+        a, b = b, a + b
+    print(fib_sequence)
+
